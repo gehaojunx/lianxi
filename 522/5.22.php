c68c2bccb2c548a844b549f309d240e1137c24ff
gehuijun <gehuijun@163.com>
2019-05-23 08:41:58 +0800

    20190523

--- 522/5.22.php
+++
@@ -1,8 +0,0 @@
-<?php 
-
- function ReverseSentence($str)
-{
-	$str = "student. a am I";
-	$res = strrev($str);
-	print_r($res);
-}
---
+++ 523/5.23.php
@@ -0,0 +1,8 @@
+<?php 
+	function Sum_Solution($n)
+	{
+		for ($n=0;$n<5;$n++)
+		{ 
+			return $n;
+		}
+	}
